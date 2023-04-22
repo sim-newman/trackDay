7735dd54b4438856fd14913b0e215d81cacd1693
simonnewman <sim.newman@me.com>
2023-04-22 21:43:37 +0100

    Add track group logic

--- createLists.py
+++ createLists.py
@@ -14,19 +14,29 @@ oExcel_loc = '/Users/simonnewman/Dropbox/documents/pb/output/trackdays_output_'+
 
 print(simple_colors.red('Starting: Read excel file ' + iExcel_loc))  
 # Load the Excel file
-excel_file = pd.read_excel(iExcel_loc)
+excel_df = pd.read_excel(iExcel_loc)
+
+# Modify values in the DataFrame
+# df.loc[df['column_name'] == 'value_to_replace', 'column_name'] = 'new_value'
+
+# Modify values in the DataFrame
+# excel_df.loc[excel_df['Lineitem name'] == 'sim.newman@me.com', 'Lineitem name'] = 'sim.newman@me.com.replaced'
+excel_df.loc[excel_df['Lineitem name'].str.contains('Fast'), 'Lineitem name'] = 'Fast'
+excel_df.loc[excel_df['Lineitem name'].str.contains('Intermediate'), 'Lineitem name'] = 'Itermidiate'
+excel_df.loc[excel_df['Lineitem name'].str.contains('Novice'), 'Lineitem name'] = 'Novice'
+
 
 # Select specific columns by name
-selected_columns = excel_file[['Lineitem name','Name', 'Billing Name','Email', 'Phone']]
+selected_columns = excel_df[['Lineitem name','Name', 'Billing Name','Email', 'Phone']]
 
 # Output the selected columns
 print(selected_columns)
 
 # Read the input file
-excel_filtered = pd.read_excel(iExcel_loc, usecols=selected_columns)
+excel_filtered_df = pd.read_excel(iExcel_loc, usecols=selected_columns)
 
 # Write the output file
-excel_filtered.to_excel(oExcel_loc, index=False)
+excel_filtered_df.to_excel(oExcel_loc, index=False)
 
 print(simple_colors.red('Finished: Results written to ' + oExcel_loc  ))  
 
