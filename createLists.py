509765085dcc5ff0f950674857861e30a00c273b
simonnewman <sim.newman@me.com>
2023-04-23 15:30:33 +0100

    Split Lineitem Name into TD Date and Group

--- createLists.py
+++ createLists.py
@@ -1,9 +1,9 @@
-import datetime
+import datetime as dt
 import pandas as pd
-import simple_colors
+import simple_colors as sc
 
 # create a datetime object for the current date and time
-now = datetime.datetime.now() 
+now = dt.datetime.now() 
 
 # convert the datetime object to a string
 date_string = now.strftime("%Y-%m-%d_%H%M%S") 
@@ -12,15 +12,11 @@ print (date_string)
 iExcel_loc = '/Users/simonnewman/Dropbox/documents/pb/trackdays.xlsx'
 oExcel_loc = '/Users/simonnewman/Dropbox/documents/pb/output/trackdays_output_'+ date_string +'.xlsx'
 
-print(simple_colors.red('Starting: Read excel file ' + iExcel_loc))  
+print(sc.red('Starting: Read excel file ' + iExcel_loc))  
 # Load the Excel file
 excel_df = pd.read_excel(iExcel_loc)
 
 # Modify values in the DataFrame
-# df.loc[df['column_name'] == 'value_to_replace', 'column_name'] = 'new_value'
-
-# Modify values in the DataFrame
-# excel_df.loc[excel_df['Lineitem name'] == 'sim.newman@me.com', 'Lineitem name'] = 'sim.newman@me.com.replaced'
 excel_df.loc[excel_df['Lineitem name'].str.contains('Fast'), 'Lineitem name'] = 'Fast'
 excel_df.loc[excel_df['Lineitem name'].str.contains('Intermediate'), 'Lineitem name'] = 'Itermidiate'
 excel_df.loc[excel_df['Lineitem name'].str.contains('Novice'), 'Lineitem name'] = 'Novice'
@@ -28,9 +24,13 @@ excel_df.loc[excel_df['Lineitem name'].str.contains('Novice'), 'Lineitem name'] 
 # sort by group
 excel_df = excel_df.sort_values(by='Lineitem name', ascending=False)
 
+# rename tjhe lineitem to Group
+#excel_df = excel_df.rename(columns={'Lineitem name': 'Group'})
 
 # Select specific columns by name
 selected_columns = excel_df[['Lineitem name','Name', 'Billing Name','Email', 'Phone']]
+
+
 
 # Output the selected columns
 print(selected_columns)
@@ -39,16 +39,28 @@ print(selected_columns)
 excel_filtered_df = pd.read_excel(iExcel_loc, usecols=selected_columns)
 
 # Modify values in the DataFrame
-# excel_df.loc[excel_df['Lineitem name'] == 'sim.newman@me.com', 'Lineitem name'] = 'sim.newman@me.com.replaced'
-excel_filtered_df.loc[excel_filtered_df['Lineitem name'].str.contains('Fast'), 'Lineitem name'] = 'Fast'
-excel_filtered_df.loc[excel_filtered_df['Lineitem name'].str.contains('Intermediate'), 'Lineitem name'] = 'Itermidiate'
-excel_filtered_df.loc[excel_filtered_df['Lineitem name'].str.contains('Novice'), 'Lineitem name'] = 'Novice'
+# excel_filtered_df.loc[excel_filtered_df['Lineitem name'].str.contains('Fast'), 'Lineitem name'] = 'Fast'
+# excel_filtered_df.loc[excel_filtered_df['Lineitem name'].str.contains('Intermediate'), 'Lineitem name'] = 'Intermediate'
+# excel_filtered_df.loc[excel_filtered_df['Lineitem name'].str.contains('Novice'), 'Lineitem name'] = 'Novice'
+
+##df[['Column A', 'Column B']] = df['Column'].str.split(' ', 1, expand=True)
+#df[['Column A', 'Column B']] = df['Column'].str.split(' ', n=1, expand=True)
+
+# fill missing values with empty strings
+excel_filtered_df['Lineitem name'].fillna('', inplace=True)
+excel_filtered_df[['Date of TD', 'Group']] = excel_filtered_df['Lineitem name'].str.split('-', n=1, expand=True)
+
 
 # sort by group
-excel_filtered_df = excel_filtered_df.sort_values(by='Lineitem name', ascending=False)
+#excel_filtered_df = excel_filtered_df.sort_values(by='Lineitem name', ascending=False)
+
+# Rename columns
+excel_filtered_df = excel_filtered_df.rename(columns={'Name': 'Ref'})
+#excel_filtered_df = excel_filtered_df.rename(columns={'Lineitem name': 'Group'})
+
 
 # Write the output file
 excel_filtered_df.to_excel(oExcel_loc, index=False)
 
-print(simple_colors.red('Finished: Results written to ' + oExcel_loc  ))  
+print(sc.red('Finished: Results written to ' + oExcel_loc  ))  
 
