57c35b036746a4cb3bd3aca9764050d89229b9a8
simonnewman <sim.newman@me.com>
2023-04-28 10:44:51 +0100

    Externalise the config settings

--- createLists.py
+++ createLists.py
@@ -1,23 +1,28 @@
 import datetime as dt
 import pandas as pd
 import simple_colors as sc
+import yaml
 
+# load the application properties
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
 # create a datetime object for the current date and time
 now = dt.datetime.now() 
 
 # convert the datetime object to a string
-date_string = now.strftime("%Y-%m-%d_%H%M%S") 
+timestamp = now.strftime("%Y-%m-%d_%H%M%S") 
 
 # file locations for input/output excels
-iExcel_loc = '/Users/simonnewman/Dropbox/documents/pb/bookings2.xlsx'
-oExcel_loc = '/Users/simonnewman/Dropbox/documents/pb/output/trackdays_output_'+ date_string +'.xlsx'
+iExcel_loc = config['excel']['input_location'] + config['excel']['input_filename']
+oExcel_loc = config['excel']['output_location'] + config['excel']['output_filename'].format(
+    timestamp = timestamp
+)
 
 print(sc.red('Starting: Read excel file ' + iExcel_loc))  
 
 # pick columns to be in report.
 sColumns = ['Lineitem name','Created at','Name', 'Billing Name','Email','Notes']
-
 
 # Read the input file into a dataframe
 filtered_df = pd.read_excel(iExcel_loc, usecols=sColumns)
