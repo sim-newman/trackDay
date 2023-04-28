8bf85e1133e1f3a1d6b98a0fe153d2b431e4ea74
simonnewman <sim.newman@me.com>
2023-04-28 14:25:17 +0100

    externalise config file and make yaml

--- createLists.py
+++ createLists.py
@@ -1,25 +1,27 @@
 import datetime as dt
 import pandas as pd
-import simple_colors as sc
 import yaml
+import logging.config
 
 # load the application properties
-with open('config.yaml', 'r') as f:
-    config = yaml.safe_load(f)
+with open('config.yaml', 'r') as fapp:
+    appconfig = yaml.safe_load(fapp)
 
-# create a datetime object for the current date and time
+# Load the logging configuration from a YAML file
+# Configure the logging module using a YAML file
+logging.config.dictConfig(yaml.load(open('logging.yaml'), Loader=yaml.FullLoader))
+
+# create a datetime object for the current date and time and convert to string.
 now = dt.datetime.now() 
-
-# convert the datetime object to a string
 timestamp = now.strftime("%Y-%m-%d_%H%M%S") 
 
 # file locations for input/output excels
-iExcel_loc = config['excel']['input_location'] + config['excel']['input_filename']
-oExcel_loc = config['excel']['output_location'] + config['excel']['output_filename'].format(
+iExcel_loc = appconfig['excel']['input_location'] + appconfig['excel']['input_filename']
+oExcel_loc = appconfig['excel']['output_location'] + appconfig['excel']['output_filename'].format(
     timestamp = timestamp
 )
 
-print(sc.red('Starting: Read excel file ' + iExcel_loc))  
+logging.info ('Starting: Read excel file ' + iExcel_loc)
 
 # pick columns to be in report.
 sColumns = ['Lineitem name','Created at','Name', 'Billing Name','Email','Notes']
@@ -64,13 +66,13 @@ for index, row in sorted_df.iterrows():
     
     if name_str == 'nan':
         try:
-          print(sc.blue('We do not have a name for ' + str(row['Email']) + ' we need to auto populate with ' + str(hashmap[email[0]])))
+          logging.debug('We do not have a name for ' + str(row['Email']) + ' we need to auto populate with ' + str(hashmap[email[0]]))
           row['Name'] = str(hashmap[email[0]])
         except KeyError:
-            print('Something went wrong! for email address lookup')
+            logging.error('Something went wrong! for email address lookup')
 
 # Write the output file
 sorted_df.to_excel(oExcel_loc, index=False) 
 
-print(sc.red('Finished: Results written to ' + oExcel_loc  ))  
+logging.info('Finished: Results written to ' + oExcel_loc  )
 
